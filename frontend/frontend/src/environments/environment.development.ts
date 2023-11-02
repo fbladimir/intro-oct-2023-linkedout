f532cc787cd7d5f0e0b2f41cebffe05f3985f69b
Franklin CanoKuntz <franklin_b_cano-kuntz@progressive.com>
2023-11-02 13:42:46 -0400

    added functionality to link page

--- frontend/frontend/src/environments/environment.development.ts
+++ frontend/frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: "http://localhost:1338/",
-    authAuthority: "http://localhost:9999/default",
-  };
+  apiUrl: "http://localhost:1338/",
+  authAuthority: "http://localhost:9999/default",
+};
